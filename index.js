840b435c86bfd4c18f3f270e53ec495befc24af6
rashidsarkar <rashidrock558@gmail.com>
2023-11-14 16:06:47 +0600

    Added cart collection and endpoints

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydmxw3q.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewsCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("cart");
     // await client.connect();
 
     app.get("/menu", async (req, res) => {
@@ -35,6 +36,26 @@ async function run() {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
+
+    // card collection
+    app.post("/api/addTocarts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.get("/api/allCartsItem", async (req, res) => {
+      const emailQuery = req.query.email;
+      const query = { email: emailQuery };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/api/cartsDelete/:id", async (req, res) => {
+      const idQuery = req.params.id;
+      const query = { _id: new ObjectId(idQuery) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
 
     await client.db("admin").command({ ping: 1 });
